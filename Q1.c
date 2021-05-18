87812211d23c2e05b5eec4546c2211bb8ad68762
RahulRustagi <77167720+rrustagi20@users.noreply.github.com>
2021-05-18 23:11:13 +0530

    New Update Answer1

--- Q1.c
+++ Q1.c
@@ -2,11 +2,11 @@
 int main()
 {
     printf("What is your name?\n");
-    char s[100];
+    char s[100]; //default memory allocation.
     scanf("%[^\n]",s);
     printf("Hello, %s\nWhat year were you born?\n",s);
     int year;
     scanf("%d",&year);
-    printf("Congratulations, you are %d old!\n",(2018-year));
+    printf("Congratulations, you are %d old!\n",(2018-year)); //calculating age wrt year 2018.
     return 0;
 }
